20abc9b5145fb4a1c2f1fd6afb1ae329ebb06e9a
DMiniKim <angks1212@naver.com>
2025-07-09 15:32:51 +0900

    (2025.7.9) 이중 연결 리스트

---
+++ Program/Class 01th ( Single Linked List )/Source.cpp
@@ -0,0 +1,159 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+template <typename T>
+class List
+{
+private:
+	int size;
+	struct Node
+	{
+		Node* next;
+		T data;
+		~Node()
+		{
+			cout << "삭제되었습니다" << endl;
+		}
+	};
+	Node* head;
+public:
+	List()
+	{
+		size = 0;
+		head = nullptr;
+	}
+	~List()
+	{
+		while (head != nullptr)
+		{
+			Node* delNode = head;
+			head = delNode->next;
+			delete delNode;
+			//pop_front(); <- 딸깍
+		}
+	}
+	void push_front(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		if ((head == nullptr) && size == 0)
+		{
+			head = newNode;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			newNode->next = head;
+			head = newNode;
+		}
+		size++;
+	}
+	void pop_front()
+	{
+		Node* deleteNode = head;
+		if (deleteNode == nullptr && size == 0)
+			cout << "비어있습니다" << endl;
+		else
+		{
+			head = deleteNode->next;
+			delete deleteNode;
+			size--;
+		}
+
+	}
+	void push_back(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		if (head == nullptr && size == 0)
+		{
+			head = newNode;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			Node* tailNode = head;
+			while (tailNode->next != nullptr)
+			{
+				tailNode = tailNode->next;
+			}
+			tailNode->next = newNode;
+		}
+		size++;
+	}
+	void pop_back()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is Empty" << endl;
+		}
+		else
+		{
+			Node* delNode = head;
+			Node* prevNode = head;
+			while (delNode->next != nullptr)
+			{
+				prevNode = delNode;
+				delNode = delNode->next;
+			}
+			if (size == 1)
+			{
+				head = nullptr;
+			}
+			prevNode->next = nullptr;
+			delete delNode;
+			size--;
+		}
+	}
+	bool empty()
+	{
+		return (head == nullptr);
+	}
+	void Remove(T data)
+	{
+		if (head == nullptr)
+		{
+			return;
+		}
+		else
+		{
+			Node* delNode = head;
+			Node* preNode = head;
+			while (preNode != nullptr)
+			{
+				if (delNode->data == data)
+				{
+					if (delNode == head)
+					{
+						head = delNode->next;
+					}
+					else
+					{
+						preNode->next = delNode->next;
+					}
+					delete delNode;
+					size--;
+					delNode = head;
+				}
+				preNode = delNode;
+				delNode = delNode->next;
+			}
+		}
+	}
+
+};
+
+
+int main()
+{
+	List<int> list;
+	list.push_front(10);
+	list.push_front(20);
+
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-
-
 template <typename T>
 class List
 {
@@ -11,117 +9,28 @@ private:
 	int size;
 	struct Node
 	{
-		Node* next;
 		T data;
-		~Node()
-		{
-			cout << "삭제되었습니다" << endl;
-		}
+		Node* prev = nullptr;
+		Node* next = nullptr;
 	};
 	Node* head;
+	Node* tail;
+
 public:
 	List()
 	{
 		size = 0;
 		head = nullptr;
+		tail = nullptr;
 	}
-	~List()
-	{
-		while (head != nullptr)
-		{
-			Node* delNode = head;
-			head = delNode->next;
-			delete delNode;
-			//pop_front(); <- 딸깍
-		}
-	}
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		if ((head == nullptr) && size == 0)
-		{
-			head = newNode;
-			newNode->next = nullptr;
-		}
-		else
-		{
-			newNode->next = head;
-			head = newNode;
-		}
-		size++;
-	}
-	void pop_front()
-	{
-		Node* deleteNode = head;
-		if (deleteNode == nullptr&& size == 0)
-			cout << "비어있습니다" << endl;
-		else
-		{
-			head = deleteNode->next;
-			delete deleteNode;
-			size--;
-		}
 
-	}
-	void push_back(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		if (head == nullptr&&size == 0)
-		{
-			head = newNode;
-			newNode->next = nullptr;
-		}
-		else
-		{
-			Node* tailNode = head;
-			while (tailNode->next != nullptr)
-			{
-				tailNode = tailNode->next;
-			}
-			tailNode->next = newNode;
-		}
-		size++;
-	}
-	void pop_back()
-	{
-		if (head == nullptr)
-		{
-			cout << "List is Empty" << endl;
-		}
-		else
-		{
-			Node* delNode = head;
-			Node* prevNode = head;
-			while (delNode->next != nullptr)
-			{
-				prevNode = delNode;
-				delNode = delNode->next;
-			}
-			if (size == 1)
-			{
-				head = nullptr;
-			}
-			prevNode->next = nullptr;
-			delete delNode;
-			size--;
-		}
-	}
-	bool empty()
-	{
-		return (head == nullptr);
-	}
 };
+
 
 
 int main()
 {
 	List<int> list;
-	list.push_front(10);
-	list.push_front(20);
-
 
 	return 0;
 }
