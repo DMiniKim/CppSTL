3cb60ba80f08815ef5c12544bab6b1398af558b4
DMiniKim <angks1212@naver.com>
2025-07-29 15:40:19 +0900

    (2025.7.29) 그래프의 표현

---
+++ Program/Class 11th ( Adjacency Matrix )/Source.cpp
@@ -0,0 +1,177 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+
+// 그래프
+// 방향 그래프		-> 특정한 방향으로 연결된 그래프
+// 무방향 그래프	-> 양방향으로 연결된 그래프
+// 가중치 그래프	-> 간선에 가중치가 있는 그래프	
+
+class ADjacencyMatrix
+{
+private:
+	T* vertex;				// 정점의 집합체
+	int capacity;			// 그래프의 크기
+	int size;		        // 정점의 개수	
+
+	int matrixCount;		// 인접 행렬의 개수
+	int** matrix;
+public:
+	ADjacencyMatrix()
+	{
+		size = 0;
+		capacity = 0;
+		matrixCount = 0;
+		vertex = nullptr;
+		matrix = nullptr;
+	}
+	void push(T data)
+	{
+		if (0 >= capacity)
+		{
+			resize(1);
+		}
+		else if (size >= capacity)
+		{
+			resize(capacity * 2);
+		}
+		vertex[size++] = data;
+	}
+	void resize(int newSize)
+	{
+		capacity = newSize;
+		T* temp = new T[capacity];
+
+		for (int i = 0; i < capacity; i++)
+		{
+			temp[i] = NULL;
+		}
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = vertex[i];
+		}
+		if (vertex != nullptr)
+		{
+			delete[] vertex;
+		}
+		vertex = temp;
+	}
+	void resize()
+	{
+		// 인접행렬의 메모리를 새로 생성하는 함수
+		int** newMatrix = new int* [size];
+		for (int i = 0; i < size; i++)
+		{
+			newMatrix[i] = new int[size]();
+		}
+		for (int i = 0; i < matrixCount; i++)
+		{
+			for (int j = 0; j < matrixCount; j++)
+			{
+				newMatrix[i][j] = matrix[i][j];
+			}
+		}
+		if (matrix != nullptr)
+		{
+			for (int i = 0; i < matrixCount; i++)
+			{
+				delete[] matrix[i];
+			}
+			delete[] matrix;
+		}
+		matrix = newMatrix;
+		matrixCount = size;
+	}
+
+	void edge(int i, int j)
+	{
+		// 1. vertex가 없는 상태에서 연결하면 
+		//    인접 행렬이 없다고 나와야 함.
+		if (vertex == nullptr)
+		{
+			cout << "Not Found Matrix" << endl;
+		}
+		// 2. vertex의 크기 범위를 벗어나게되면 
+		//    연결이 되지 않도록 설정해야 함.
+		else if (i >= size || j >= size)
+		{
+			cout << "Out Of range" << endl;
+		}
+		// 3. 인접 행렬이 없을 때 정점의 크기만큼 
+		//    인접행렬을 생성함.
+		else
+		{
+			if (matrix == nullptr)
+			{
+				matrixCount = size;
+				matrix = new int* [size];
+				for (int i = 0; i < size; i++)
+				{
+					matrix[i] = new int[size]();
+				}
+				matrix[j][i] = 1;
+				matrix[i][j] = 1;
+			}
+			else if (matrixCount < size)
+			{
+				resize();
+				matrix[j][i] = 1;
+				matrix[i][j] = 1;
+			}
+			else
+			{
+				matrix[j][i] = 1;
+				matrix[i][j] = 1;
+			}
+		}
+	}
+
+	// 인접 행렬을 출력하는 함수
+	template <typename T>
+	friend ostream& operator <<(ostream& o, const ADjacencyMatrix<T>& matrix);
+	~ADjacencyMatrix()
+	{
+		if (matrix != nullptr)
+		{
+			for (int i = 0; i < matrixCount; i++)
+			{
+				delete[] matrix[i]
+			}
+			delete[] matrix;
+		}
+		delete vertex;
+	}
+};
+
+
+
+int main()
+{
+	ADjacencyMatrix<int> matrix;
+	matrix.push(5);
+	matrix.push(7);
+	matrix.push(12);
+	//matrix.edge(1, 0);
+	//matrix.edge(0, 2);
+
+
+	//cout << matrix << endl;
+
+	return 0;
+
+}
+
+template <typename T>
+ostream& operator << (ostream& o, const ADjacencyMatrix<T>& matrix)
+{
+	for (int i = 0; i < matrix.matrixCount; i++)
+	{
+		for (int j = 0; j < matrix.matrixCount; j++)
+		{
+			o << matrix.matrix[i][j] << " ";
+		}
+		o << endl;
+	}
+	return o;
+}
